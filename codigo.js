73a23bc24e4db0644bf16206df2f4a0cc2f8ad86
dianaaltamirano <nathalyusl@gmail.com>
2021-09-16 22:16:14 -0500

    Color green

--- codigo.js
+++ codigo.js
@@ -1,14 +1,28 @@
-alert("Recordatorio")
+alert("Recordatorio");
 recipiente = "papel";
 
-alert(recipiente)
+alert(recipiente);
 
 <!-- Tipos datos -->
 
-string = "cadena de texto"
-number = 4
-boolean = "false true"
-'0 y 1'
+string = "cadena de texto";
+number = 4;
+boolean = "false true";
+'0 y 1';
 
-let number = 0
+let number;
+number = 4;
 const = "usl"
+
+let number1, number2, number3;
+
+let number1 = 2;
+let number2 = 4;
+
+let number = null;
+
+alert = (number1 * number2);
+
+let nombre prompt("Cuál es tu nombre?");
+alert("Hola" + nombre);
+
